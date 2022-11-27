5cbfe8f13b7d8909f32d640b4f8054a1aff3b02c
An NGUYEN <dinhiendung1026@gmail.com>
2022-11-28 01:50:59 +0700

    Fix UI cart

--- src/components/Modal/Cart/CartModal.jsx
+++ src/components/Modal/Cart/CartModal.jsx
@@ -1,129 +1,210 @@
-import { React, useEffect, useState, useRef } from 'react'
+import { React, useEffect, useState, useRef } from "react";
 import { Row, Col, Container, Modal, Button } from "react-bootstrap";
-import './CartModal.css'
-import { hasLogin } from '../../../database/Auth/Auth';
-import icon_google from '../../../assets/images/arm-chair-01.jpg'
+import "./CartModal.css";
+import { hasLogin } from "../../../database/Auth/Auth";
+import icon_google from "../../../assets/images/arm-chair-01.jpg";
 
 function Divide() {
-    return (<>
-        <div className="d-flex py-2 px-5">
-            <hr className="my-auto flex-grow-1"></hr>
-        </div>
+  return (
+    <>
+      <div className="d-flex">
+        <hr className="my-auto flex-grow-1"></hr>
+      </div>
     </>
-    )
+  );
 }
 
 function ProductCart(props) {
-    const [quantity, setQuantity] = useState(props.item.quantity)
+  const [quantity, setQuantity] = useState(props.item.quantity);
 
-    const updateQuantity = (value) => {
-        setQuantity(value)
-        props.item.quantity = value
-        props.update()
-    }
+  const updateQuantity = (value) => {
+    setQuantity(value);
+    props.item.quantity = value;
+    props.update();
+  };
 
-    const removeItem = (item) => {
-        props.remoteItem(item)
-    }
+  const removeItem = (item) => {
+    props.remoteItem(item);
+  };
 
-    return (<Row className='p-2 g-0'>
-        <img src={icon_google} alt="" className='col-2 img-fluid' />
-        <Col className='my-auto'>
-            <div className='text-primary'>{props.item.name}</div>
-            <div className='text-secondary'>{props.item.id}</div>
-        </Col>
-        <div className='col-3 my-auto'>
-            <i className='ri-subtract-line col-3 rounded btn-quantity' onClick={() => updateQuantity(quantity - 1)}></i>
-            <input type="number" className='col-6 px-1 mx-1 rounded border border-1' value={quantity} onChange={(e) => updateQuantity(parseInt(e.target.value))} />
-            <i className='ri-add-line col-3 rounded btn-quantity' onClick={() => updateQuantity(quantity + 1)}></i>
+  return (
+    <Row className="d-flex align-items-center product-cart my-3 justify-content-between">
+      <Col
+        xs="10"
+        md="6"
+        className="product-cart__img d-flex align-items-center order-0"
+      >
+        <img src={icon_google} alt="" className="img-fluid" />
+        <div className="">
+          <p className="text-secondary">{props.item.id}</p>
+          <p className="text-black fw-bold ">{props.item.name}</p>
         </div>
-        <span className='col-2 my-auto'>{quantity * props.item.pricePerUnit}</span>
-        <i className='col-1 ri-close-line my-auto' onClick={() => removeItem(props.item)}></i>
-    </Row>)
+      </Col>
+      <Col xs="6" md="auto" className="d-flex justify-content-center order-2">
+        <span>
+          <i
+            className="ri-subtract-line rounded btn-quantity"
+            onClick={() => updateQuantity(quantity - 1)}
+          ></i>
+        </span>
+        <input
+          type="number"
+          className="rounded border border-1 ps-3 fw-light fs-6"
+          style={{ width: "4rem" }}
+          value={quantity}
+          onChange={(e) => updateQuantity(parseInt(e.target.value))}
+        />
+        <span>
+          <i
+            className="ri-add-line rounded btn-quantity"
+            onClick={() => updateQuantity(quantity + 1)}
+          ></i>
+        </span>
+      </Col>
+      <Col xs="6" md="auto" className="text-end pe-5 p-md-0 ps-xl-5 order-3">
+        <p className="">${quantity * props.item.pricePerUnit}</p>
+      </Col>
+      <Col xs="2" md="auto" className="order-1 order-md-last">
+        <span>
+          <i
+            className="ri-close-line"
+            onClick={() => removeItem(props.item)}
+          ></i>
+        </span>
+      </Col>
+    </Row>
+  );
 }
 
 function CartModal(props) {
-    const [methodShipping, setMethodShipping] = useState(1)
-    const [updated, setUpdated] = useState(false)
+  const [methodShipping, setMethodShipping] = useState(1);
+  const [updated, setUpdated] = useState(false);
 
-    const update = () => {
-        setUpdated(!updated)
-    }
+  const update = () => {
+    setUpdated(!updated);
+  };
 
-    const removeItem = (item) => {
-        let index = props.items.indexOf(props.items.filter(e => e.id === item.id)[0])
-        if (index >= 0) {
-            props.items.splice(index, 1)
-        }
-        update()
-    }
-
-    const checkout = () => {
-        if (hasLogin()) {
-            console.log("Checkouted")
-        }
-        else {
-            props.open()
-        }
-    }
-
-    return (
-        <Modal
-            show={props.show}
-            onHide={props.close}
-            size="xl"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-            scrollable={true}
-        >
-            <Modal.Header closeButton>
-                <Row className='col-lg-8 col-11 g-0 p-2'>
-                    <span className='col-6'>Shopping cart</span>
-                    <span className='col-6 text-end'>{props.items.length} items</span>
-                </Row>
-            </Modal.Header>
-            <Modal.Body className='p-0 font-modal'>
-                <Container>
-                    <Row>
-                        <Row className='col-lg-8 col-12 g-0 p-2'>
-                            {props.items.map(item =>
-                                <>
-                                    <ProductCart key={item.id} item={item} update={update} remoteItem={removeItem} />
-                                    <Divide />
-                                </>
-                            )}
-                        </Row>
-                        <Row className='col-lg-4 col-12 rounded-summary bg-primary-section-summary g-0 gy-lg-0 gy-2 p-3 text-white text-center max-height-summary'>
-                            <span className='h3'>Summary</span>
-                            <Divide />
-                            <span className='col-6'>{props.items.length} items</span>
-                            <span className='col-6 '>{props.items.reduce((partitalSum, value) => partitalSum + value.quantity * value.pricePerUnit, 0)}</span>
-                            <span className='col-6'>SHPPING</span>
-                            <div className='col-9 mx-auto'>
-                                <select className="form-select" aria-label="Default select example" value={methodShipping} onChange={(e) => setMethodShipping(e.target.value)}>
-                                    <option selected>Open this select menu</option>
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
-                                </select>
-                            </div>
-                            <span className='col-6'>GIFT CODE</span>
-                            <div className='col-9 mx-auto'>
-                                <div className="input-group">
-                                    <span className="input-group-text" id="addon-wrapping">GSM</span>
-                                    <input type="text" className="form-control" placeholder="Gift code" aria-label="Username" aria-describedby="addon-wrapping"></input>
-                                </div>
-                            </div>
-                            <Divide />
-                            <span className='col-6'>TOTAL PRICE</span>
-                            <span className='col-6 '>$ 532.4</span>
-                            <button className='col-12 btn btn-checkout text-uppercase' onClick={checkout}>Checkout</button>
-                        </Row>
-                    </Row>
-                </Container>
-            </Modal.Body>
-        </Modal>
+  const removeItem = (item) => {
+    let index = props.items.indexOf(
+      props.items.filter((e) => e.id === item.id)[0]
     );
+    if (index >= 0) {
+      props.items.splice(index, 1);
+    }
+    update();
+  };
+
+  const checkout = () => {
+    if (hasLogin()) {
+      console.log("Checkouted");
+    } else {
+      props.open();
+    }
+  };
+
+  return (
+    <Modal
+      show={props.show}
+      onHide={props.close}
+      size="xl"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      scrollable={true}
+    >
+      <Modal.Body className="p-0 font-modal">
+        <Container>
+          <Row className="">
+            <Col className="col-lg-8 col-12 p-xl-5 py-5">
+              <div className="d-flex justify-content-between align-items-center mb-5">
+                {" "}
+                <h1 className="display-6">Shopping cart</h1>
+                <p className="opacity-75 fw-light">
+                  {props.items.length} items
+                </p>
+              </div>
+              <Divide />
+              {props.items.map((item) => (
+                <>
+                  <ProductCart
+                    key={item.id}
+                    item={item}
+                    update={update}
+                    remoteItem={removeItem}
+                  />
+                  <Divide />
+                </>
+              ))}
+            </Col>
+            <Col className="col-lg-4 col-12 cart-modal__summary fw-bold">
+              <div className="mb-5">
+                <h3>Summary</h3>
+              </div>
+              <Divide />
+              <div className="my-4">
+                <div className="d-flex justify-content-between my-2">
+                  <p className="">ITEMS: {props.items.length}</p>
+                  <p className="">
+                    $
+                    {props.items.reduce(
+                      (partitalSum, value) =>
+                        partitalSum + value.quantity * value.pricePerUnit,
+                      0
+                    )}
+                  </p>
+                </div>
+                <div className="py-3">
+                  <p className="mb-3">SHIPPING</p>
+                  <div className="">
+                    <select
+                      className="form-select form-control"
+                      aria-label="Default select example"
+                      value={methodShipping}
+                      onChange={(e) => setMethodShipping(e.target.value)}
+                    >
+                      <option selected>Open this select menu</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="py-3">
+                  <p className="mb-3">GIFT CODE</p>
+                  <div className="">
+                    <div className="input-group">
+                      <p className="input-group-text" id="addon-wrapping">
+                        GSM
+                      </p>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Gift code"
+                        aria-label="Username"
+                        aria-describedby="addon-wrapping"
+                      ></input>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <Divide />
+              <div className="d-flex justify-content-between my-3">
+                <p className="">TOTAL PRICE</p>
+                <p className="">$ 532.4</p>
+              </div>
+
+              <button
+                className="col-12 btn btn-checkout text-uppercase"
+                onClick={checkout}
+              >
+                Checkout
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </Modal.Body>
+    </Modal>
+  );
 }
 
-export default CartModal
+export default CartModal;

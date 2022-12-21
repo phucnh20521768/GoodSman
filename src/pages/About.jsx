dd036d25b5106b0722f318686fcfd0c1ed001824
phucnh20521768 <20521768@ms.uit.edu.vn>
2022-12-21 22:23:21 +0700

    fix numbered

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -49,23 +49,23 @@ function About() {
                     </div>
                     <div className='col-md-8'>
                         <div className='text-end fw-bold waviy'>
-                            <span style={{ "--i": 1 }}>S</span>
-                            <span style={{ "--i": 2 }}>Ứ</span>
+                            <span style={{ "--i": 2 }}>S</span>
+                            <span style={{ "--i": 3 }}>Ứ</span>
                             <span style={{ "--i": 4 }} className="left">M</span>
                             <span style={{ "--i": 5 }}>Ệ</span>
                             <span style={{ "--i": 6 }}>N</span>
                             <span style={{ "--i": 7 }}>H</span>
-                            <span style={{ "--i": 9 }} className="left">C</span>
-                            <span style={{ "--i": 10 }}>Ủ</span>
-                            <span style={{ "--i": 11 }}>A</span>
-                            <span style={{ "--i": 13 }} className="left">C</span>
-                            <span style={{ "--i": 14 }}>H</span>
-                            <span style={{ "--i": 15 }}>Ú</span>
-                            <span style={{ "--i": 16 }}>N</span>
-                            <span style={{ "--i": 17 }}>G</span>
-                            <span style={{ "--i": 19 }} className="left">T</span>
-                            <span style={{ "--i": 20 }}>Ô</span>
-                            <span style={{ "--i": 21 }}>I</span>
+                            <span style={{ "--i": 8 }} className="left">C</span>
+                            <span style={{ "--i": 9 }}>Ủ</span>
+                            <span style={{ "--i": 10 }}>A</span>
+                            <span style={{ "--i": 11 }} className="left">C</span>
+                            <span style={{ "--i": 12 }}>H</span>
+                            <span style={{ "--i": 13 }}>Ú</span>
+                            <span style={{ "--i": 14 }}>N</span>
+                            <span style={{ "--i": 15 }}>G</span>
+                            <span style={{ "--i": 16 }} className="left">T</span>
+                            <span style={{ "--i": 17 }}>Ô</span>
+                            <span style={{ "--i": 18 }}>I</span>
                         </div>
                         <hr className='line-content align-end' />
                         <p className='text-justify'>ThinkPro với sứ mệnh mang lại những giá trị tốt đẹp, luôn lấy khách hàng làm trung tâm, bằng kiến thức chuyên môn, sự chân thành và nhiệt huyết của tuổi trẻ, đồng thời ứng dụng công nghệ số để tối ưu và linh hoạt, mang lại trải nghiệm tốt nhất dành cho khách hàng qua từng dịch vụ mà công ty cung cấp.
@@ -76,24 +76,24 @@ function About() {
                 <div className='p-5 align-items-center row'>
                     <div className='col-md-8'>
                         <div className='fw-bold waviy'>
-                            <span style={{ "--i": 5 }}>T</span>
-                            <span style={{ "--i": 6 }}>Ầ</span>
-                            <span style={{ "--i": 7 }}>M</span>
-                            <span style={{ "--i": 9 }} className="left">N</span>
-                            <span style={{ "--i": 10 }}>H</span>
-                            <span style={{ "--i": 11 }}>Ì</span>
-                            <span style={{ "--i": 11 }}>N</span>
-                            <span style={{ "--i": 13 }} className="left">C</span>
-                            <span style={{ "--i": 14 }}>Ủ</span>
-                            <span style={{ "--i": 17 }}>A</span>
-                            <span style={{ "--i": 13 }} className="left">C</span>
-                            <span style={{ "--i": 2 }}>H</span>
-                            <span style={{ "--i": 3 }}>Ú</span>
-                            <span style={{ "--i": 4 }}>N</span>
-                            <span style={{ "--i": 5 }}>G</span>
-                            <span style={{ "--i": 6 }} className="left">T</span>
-                            <span style={{ "--i": 7 }}>Ô</span>
-                            <span style={{ "--i": 8 }}>I</span>
+                            <span style={{ "--i": 1 }}>T</span>
+                            <span style={{ "--i": 2 }}>Ầ</span>
+                            <span style={{ "--i": 3 }}>M</span>
+                            <span style={{ "--i": 4 }} className="left">N</span>
+                            <span style={{ "--i": 5 }}>H</span>
+                            <span style={{ "--i": 6 }}>Ì</span>
+                            <span style={{ "--i": 7 }}>N</span>
+                            <span style={{ "--i": 8 }} className="left">C</span>
+                            <span style={{ "--i": 9 }}>Ủ</span>
+                            <span style={{ "--i": 10 }}>A</span>
+                            <span style={{ "--i": 11 }} className="left">C</span>
+                            <span style={{ "--i": 12 }}>H</span>
+                            <span style={{ "--i": 13 }}>Ú</span>
+                            <span style={{ "--i": 14 }}>N</span>
+                            <span style={{ "--i": 15 }}>G</span>
+                            <span style={{ "--i": 16 }} className="left">T</span>
+                            <span style={{ "--i": 17 }}>Ô</span>
+                            <span style={{ "--i": 18 }}>I</span>
                         </div>
                         <hr className='line-content' />
                         <p className='text-justify'>ThinkPro với sứ mệnh mang lại những giá trị tốt đẹp, luôn lấy khách hàng làm trung tâm, bằng kiến thức chuyên môn, sự chân thành và nhiệt huyết của tuổi trẻ, đồng thời ứng dụng công nghệ số để tối ưu và linh hoạt, mang lại trải nghiệm tốt nhất dành cho khách hàng qua từng dịch vụ mà công ty cung cấp.

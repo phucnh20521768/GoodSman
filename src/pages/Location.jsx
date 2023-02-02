02e60876eda33b41417b990a48918895fd48ae72
UIT-20521205 <dinhiendung1026@gmail.com>
2023-02-02 23:04:43 +0700

    fix

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -6,27 +6,29 @@ import "../styles/location.css";
 import LocationCard from "../components/UI/LocationCard";
 import locationData from "../assets/data/location";
 import { Breadcrumb } from "react-bootstrap";
+import Helmet from "../components/Helmet/Helmet";
 
 function Location() {
-    return (
-        <Container>
-            <div>
-                <Breadcrumb className="bread">
-                    <Breadcrumb.Item href="home">Trang chủ</Breadcrumb.Item>
-                    <Breadcrumb.Item active>Địa chỉ</Breadcrumb.Item>
-                </Breadcrumb>
-            </div>
-            <hr className="line-loc"></hr>
-            <Row className="m-3">
-                {locationData.map((item, index) => (
-                    <Col lg="4" className="p-3" key={index}>
-                        <LocationCard item={item} />
-                    </Col>
-                ))}
-            </Row>
-        </Container>
-    );
+  return (
+    <Helmet title="Location">
+      <Container>
+        <div>
+          <Breadcrumb className="bread">
+            <Breadcrumb.Item href="home">Trang chủ</Breadcrumb.Item>
+            <Breadcrumb.Item active>Địa chỉ</Breadcrumb.Item>
+          </Breadcrumb>
+        </div>
+        <hr className="line-loc"></hr>
+        <Row className="m-3">
+          {locationData.map((item, index) => (
+            <Col lg="4" className="p-3" key={index}>
+              <LocationCard item={item} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </Helmet>
+  );
 }
 
 export default Location;
-
